8d6bc317ec7562d4052154daada2a293efb7ba3b
Hrushikesh-Gangisetty <hrushikeshgangisett@gmail.com>
2026-04-04 13:13:52 +0530

    Production-Code , versioncode : 14 , version-name: 1.3.0 , activebugs: Tank empty pauseresume in automissions , removing start-button from main-map

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.kft.gcs"
         minSdk = 26
         targetSdk = 35
-        versionCode = 11
-        versionName = "1.2.0"
+        versionCode = 14
+        versionName = "1.3.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
