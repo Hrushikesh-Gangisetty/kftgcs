606714934988a92914378a90eb7342899f6f5d7c
Hrushikesh-Gangisetty <hrushikeshgangisett@gmail.com>
2026-04-22 12:38:23 +0530

    Production Build : version-code:20

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,7 +23,7 @@ android {
         applicationId = "com.kft.gcs"
         minSdk = 26
         targetSdk = 35
-        versionCode = 19
+        versionCode = 20
         versionName = "1.3.3"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
